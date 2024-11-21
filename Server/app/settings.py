cbbdc2a7bedeb782fbe9675555ef721dabcfdb18
Aaron Bromma <36652096+bioresestance@users.noreply.github.com>
2024-11-20 19:53:53 -0800

    added note on jwt secret in settings

--- Server/app/settings.py
+++ Server/app/settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     db_username: str = Field("admin", env="DB_USERNAME")
     db_password: str = Field("admin", env="DB_PASSWORD")
     db_name: str = Field("brommawoodwedding", env="DB_NAME")
-    jwt_secret: str = Field("7a622820781eff8983daebd5552995d510c674d870b5e02a332360e0e68ed985", env="JWT_SECRET")
+    jwt_secret: str = Field("7a622820781eff8983daebd5552995d510c674d870b5e02a332360e0e68ed985", env="JWT_SECRET") # Not a real secret, just a placeholder for dev
     jwt_algorithm: str = Field("HS256", env="JWT_ALGORITHM")
     jwt_expires: int = Field(900, env="JWT_EXPIRES")
     redis_url: str = Field("redis://localhost:6379", env="REDIS_URL")
