9fa55418e48acee55fd8be5efa1360d8dd8dbcb1
Aaron Bromma <aaron@bromma.dev>
2024-12-29 11:53:49 -0800

    Enhance RSVP page layout and user instructions; improve error handling and privacy policy agreement

--- Client/src/pages/rsvp.tsx
+++ Client/src/pages/rsvp.tsx
@@ -60,59 +60,65 @@ const RSVPPage: React.FC = () => {
   const agreeToPrivacyPolicy = watch("agreeToPrivacyPolicy");
 
   return (
-    <div className="flex flex-col items-center w-full min-h-screen text-white pt-[20vh] px-4 md:px-0">
-      <p className="text-2xl md:text-5xl font-bold text-black p-6 md:p-12 pr-8 md:pr-16">
-        RSVP
-      </p>
-      <p className="text-center text-black mt-4 md:mt-8">
-        Please enter your RSVP code to proceed.
-      </p>
-      <form
-        onSubmit={handleSubmit(onSubmit, onError)}
-        className="w-full max-w-md mt-8"
-      >
-        <input
-          className="w-full h-12 rounded-full border border-black text-center text-black mb-4 bg-white"
-          type="text"
-          placeholder="Enter your RSVP code"
-          {...register("rsvp_code")}
-        />
-        <p className="text-red-600 text-center mb-4">
-          {errors.rsvp_code?.message}
+    <div className="flex flex-col items-center text-center w-full min-h-screen text-black pt-[20vh] px-4 md:px-0">
+      <div className="w-[50%] mx-auto">
+        <p className="text-2xl md:text-5xl font-bold text-black p-6 md:p-12 pr-8 md:pr-16">
+          RSVP
         </p>
-        <div className="flex items-center mb-4">
-          <input
-            type="checkbox"
-            {...register("agreeToPrivacyPolicy")}
-            className="mr-2"
-          />
-          <label className="text-black">
-            I agree to the{" "}
-            <a href="/privacy" className="text-blue-600 underline">
-              privacy policy
-            </a>
-            .
-          </label>
-        </div>
-        <p className="text-red-600 text-center mb-4">
-          {errors.agreeToPrivacyPolicy?.message}
+        <p className="mt-4 md:mt-8">
+          This RSVP system will be open until April 30th, 2025. You are able to make changes to your RSVP until this date. If you have any questions or need to make changes after this date, please contact us!
         </p>
-        {isError && (
-          <p className="text-red-600 text-center mb-4">
-            That RSVP code was incorrect, please check and try again!
-          </p>
-        )}
-        <button
-          className={`w-full text-3xl border-2 rounded-full border-black font-bold py-4 px-8 ${agreeToPrivacyPolicy
-            ? "bg-blue-600/75 hover:bg-blue-800 text-white/90 hover:text-white"
-            : "bg-gray-400 text-gray-700 cursor-not-allowed"
-            }`}
-          type="submit"
-          disabled={!agreeToPrivacyPolicy}
+        <br />
+        <p className="mt-4 md:mt-8">
+          Please enter your RSVP code that was sent to you in order to proceed.
+        </p>
+        <form
+          onSubmit={handleSubmit(onSubmit, onError)}
+          className=" mt-8 w-[75%] mx-auto"
         >
-          Submit
-        </button>
-      </form>
+          <input
+            className="w-full h-12 rounded-full border border-black text-center text-black mb-4 bg-white"
+            type="text"
+            placeholder="Enter your RSVP code"
+            {...register("rsvp_code")}
+          />
+          <p className="text-red-600 text-center mb-4">
+            {errors.rsvp_code?.message}
+          </p>
+          <div className="flex items-center mb-4">
+            <input
+              type="checkbox"
+              {...register("agreeToPrivacyPolicy")}
+              className="mr-2"
+            />
+            <label className="text-black">
+              I agree to the{" "}
+              <a href="/privacy" className="text-blue-600 underline font-bold">
+                privacy policy
+              </a>
+              .
+            </label>
+          </div>
+          <p className="text-red-600 text-center mb-4">
+            {errors.agreeToPrivacyPolicy?.message}
+          </p>
+          {isError && (
+            <p className="text-red-600 text-center mb-4">
+              That RSVP code was incorrect, please check and try again!
+            </p>
+          )}
+          <button
+            className={`w-[50%] text-3xl border-2 rounded-full border-black font-bold py-4 px-8 ${agreeToPrivacyPolicy
+              ? "bg-blue-600/75 hover:bg-blue-800 text-white/90 hover:text-white"
+              : "bg-gray-400 text-gray-700 cursor-not-allowed"
+              }`}
+            type="submit"
+            disabled={!agreeToPrivacyPolicy}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
